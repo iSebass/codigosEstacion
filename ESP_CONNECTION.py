1f62d6957ea0e0cddb8f9a6ea648ba06912d4e71
iSebass <jusecofe@gmail.com>
2023-04-25 13:17:34 -0500

    Se actulizan codigos de la estacion y se verifica conectividad con el servidor

--- ESP_CONNECTION.py
+++ ESP_CONNECTION.py
@@ -9,31 +9,42 @@ PASS = "isebasss"
 class ESP32_CONNECTION():
     
     def __init__(self):
-        print("Modulo Inicializado")
+        
         self._ssid = SSID
         self._pass = PASS
+        
+        # Crear una instancia de la clase WLAN
+        self.wlan = network.WLAN(network.STA_IF)
     
     def setSSID_PASSW(self, newSSID=SSID, newPASS=PASS):
         self._ssid = newSSID
         self._pass = newPASS
     
-    def StartConnection(self):
-        # Crear una instancia de la clase WLAN
-        self.wlan = network.WLAN(network.STA_IF)
+    def status(self):
+        status = self.wlan.isconnected()
+        print("estado de la conexion: ", status)
+        return status
+    
+    def disconnect(self):
+        self.wlan.disconnect()
+    
+    def connect(self):
+        
         
         # Conectar a la red Wi-Fi
         self.wlan.active(True)
         self.wlan.connect(self._ssid, self._pass)
         
         # Esperar a que se establezca la conexión
-        time_out = 20000
+        time_out = 20
         while not self.wlan.isconnected():
             time_out -= 1
             sleep(1)
             if time_out == 0:
                 print("Error Trying Connection")
                 return False
-        print("Conexion establecida")
+        print("Conexion establecida con la red: \n\n")
+        print("\t\t\tSSID: ",self._ssid,"\n\n")
         return True
     
     
@@ -41,3 +52,4 @@ class ESP32_CONNECTION():
         
 
         
+
